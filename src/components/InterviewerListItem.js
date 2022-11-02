c94a3f8656c64d4dc4f451cac06124f71b2c57f2
Sam <samjulian6@gmail.com>
2022-11-02 00:21:53 -0700

    Add InterviewerList with story and styling

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,7 +3,7 @@ import React from "react";
 import "components/InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
-  const interviewerClass = classNames('interviewers__item', {'interviewers__item--selected': props.selected})
+  const interviewerClass = classNames('interviewers__item', {'interviewers__item--selected': props.selected});
   return (
     <li className={interviewerClass} onClick={() => props.setInterviewer(props.id)}>
       <img
