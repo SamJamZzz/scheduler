f51e3c066474e762dc0079042b8c7fff0414c8bb
Sam <samjulian6@gmail.com>
2022-11-03 18:08:39 -0700

    Use axios to make GET request to obtain data on days and delete mock data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   "1": {
@@ -62,6 +45,14 @@ const appointments = {
 
 export default function Application(props) {
   const [day, onChange] = useState('Monday');
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    const url = 'http://localhost:8001/api/days';
+    axios.get(url)
+    .then(response => setDays(response.data))
+  }, [])
+
   const appointmentsArray = Object.values(appointments).map(appointment => (
     <Appointment
       key={appointment.id}
