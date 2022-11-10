06b5e799558bb56f9ac053e07752d90d13cff33f
Sam <samjulian6@gmail.com>
2022-11-09 23:44:32 -0800

    Add additional E2E tests for Cypress

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -17,7 +17,7 @@ export default function DayListItem(props) {
     }
   };
   return (
-    <li className={dayClass} onClick={event => props.setDay(props.name)}>
+    <li className={dayClass} onClick={event => props.setDay(props.name)} data-testid="day">
       <h2 className="text--regular">{props.name}</h2> 
       {formatSpots(props.spots)}
     </li>
