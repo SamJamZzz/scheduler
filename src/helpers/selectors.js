15d366c8d11b1099af9bb3d247164814eda8c99a
Sam <samjulian6@gmail.com>
2022-11-21 16:18:34 -0800

    Fix bugs and refactor selectors

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -9,31 +9,15 @@ export function getAppointmentsForDay(state, day) {
 }
 
 export function getInterviewersForDay(state, day) {
-  const foundDay = state.days.find(thisDay => thisDay.name === day);
-  if (!foundDay) {
-    return [];
-  }
-
-  const interviews = foundDay.appointments.map(appointmentId => state.appointments[appointmentId].interview).filter(interview => interview);
-  const interviewerIds = {};
-  interviews.forEach(interview => {
-    if (!interviewerIds[interview.interviewer]) {
-      interviewerIds[interview.interviewer] = true;
-    }
-  })
-  const result = Object.keys(interviewerIds).map(interviewerId => state.interviewers[interviewerId]);
-  return result;
+  return Object.values(state.interviewers)
 }
 
 export function getInterview(state, interview) {
   if (!interview) {
     return null;
   }
-  const appointmentsArray = Object.values(state.appointments).filter(appointment => appointment.interview);
-  let interviewObj = appointmentsArray.find(appointment => appointment.interview.student === interview.student && appointment.interview.interviewer === interview.interviewer).interview;
-  if (!interviewObj) {
-    return null;
+  return {
+    ...interview,
+    interviewer: state.interviewers[interview.interviewer]
   }
-  const result = {student: interviewObj.student, interviewer: state.interviewers[interviewObj.interviewer]};
-  return result;
 }
